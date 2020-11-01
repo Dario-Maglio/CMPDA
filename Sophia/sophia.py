35728568a953407613e39f1d45476e76bf48e263
Dario-Caf <cafasso.dario@gmail.com>
2020-11-01 21:02:03 +0100

    First input sophia project.

---
+++ Sophia/sophia.py
@@ -0,0 +1,9 @@
+"""Excercising with classes and neural networks."""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+class NeuralCluster:
+    """General neural cluster"""
+
+    def __init__(self, )
