7c685bfd8a0faa0bc20873b278f46bbf5d358083
Dario-Caf <cafasso.dario@gmail.com>
2021-11-15 18:03:29 +0100

    Yolo and fun

--- Extra/Workflow/execute.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.system('gfortran flibrary.f90 map_henon.f90 -o drive.x')
-os.system('./drive.x')
---
+++ cmpda/mylib/darknetyolo.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""Interface to use Yolov3"""
+
+import os
+import argparse
+
+
+def process(inpfile, thresh):
+    """File preocessing"""
+
+    ipath = os.getcwd()
+    if not os.path.exists('{0}/{1}'.format(ipath,inpfile)):
+        return 1
+    if not inpfile.endswith('.jpg'):
+        return 2
+
+    os.system('cp {0} ~/github/darknet/data'.format(inpfile))
+    os.chdir('/home/dario/github/darknet')
+    os.system('./darknet detector test cfg/coco.data cfg/yolov3.cfg yolov3.weights data/{0} -thresh {1}'.format(inpfile, thresh))
+    os.system('cp predictions.jpg {0}'.format(ipath))
+    os.system('rm predictions.jpg')
+    os.chdir('/home/dario/github/darknet/data')
+    os.system('rm {0}'.format(inpfile))
+    os.chdir(ipath)
+    os.system('xdg-open predictions.jpg')
+
+    return 0
+
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument('inpfile', type=str, help='Path to the input file')
+    PARSER.add_argument('-t', '--thresh', action='store_true', help='thresh')
+    ARGS = PARSER.parse_args()
+
+    if ARGS.thresh:
+        thresh = float(input('Set the thresh level: '))
+    else:
+        thresh = 0.25
+
+    if ((thresh>1)&(thresh<100)):
+        thresh = thresh/100
+    else:
+        print('Level not allowed. Default level selected: 25%.')
+        thresh = 0.25
+
+    STAT = process(ARGS.inpfile, thresh)
+    if STAT == 0:
+        print('The work is done.')
+    elif STAT == 1:
+        print("The file doesn't exist.")
+    elif STAT == 2:
+        print('Wrong extension: jpg format is needed.')
